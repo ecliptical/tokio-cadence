69e90a7429c1f6c732e3cdf8352700368694bc91
Peter Nehrer <pnehrer@eclipticalsoftware.com>
2020-10-28 22:07:11 -0400

    Update docs

--- README.md
+++ README.md
@@ -1,3 +1,76 @@
-# Metric Sink for Cadence built on Tokio
+# Metric Sinks for Cadence built on Tokio
 
-WIP
+[![Crates.io](https://img.shields.io/crates/v/tokio-cadence.svg)](https://crates.io/crates/tokio-cadence/)
+[![Docs.rs](https://docs.rs/tokio-cadence/badge.svg)](https://docs.rs/tokio-cadence/)
+
+A collection of [Cadence](https://crates.io/crates/cadence/) [Metric Sink](https://docs.rs/cadence/0.22/cadence/trait.MetricSink.html)
+implementations that process metrics asynchronously using [Tokio](https://crates.io/crates/tokio/).
+
+The Statsd client provided by Cadence does not support asynchronous operation -- submitting a metric may in fact block the caller!
+
+This is undesirable in asynchronous contexts.
+
+The Metric Sinks implemented in this crate alleviate this issue by allowing the application code to enqueue
+metrics without blocking, and offloading their actual sending to a separate asynchronous task.
+
+## Features
+
+- emit metrics without blocking by utilizing a non-blocking, buffered channel
+- batch multiple metrics, up to a maximum delay (or when the batch buffer fills up)
+- process metrics asynchronously in a Tokio task pool of the application's choice
+
+## Installation
+
+Add `cadence`, `tokio`, and `tokio-cadence` to your `Cargo.toml`:
+
+```toml
+[dependencies]
+cadence = "0.22"
+tokio = { version = "0.2", features = ["full"] }
+tokio-cadence = "0.1"
+```
+
+## Usage
+
+The Metric Sink constructors return a tuple consisting of the sink instance itself as well as a future,
+which the application must drive to completion; e.g., spawn it in a Tokio task pool.
+
+```rust
+use cadence::prelude::*;
+use cadence::{StatsdClient, DEFAULT_PORT};
+use tokio_cadence::TokioBatchUdpMetricSink;
+use tokio::{spawn, net::UdpSocket};
+
+#[tokio::main]
+async fn main() -> cadence::MetricResult<()> {
+    let host = ("metrics.example.com", DEFAULT_PORT);
+    let socket = UdpSocket::bind("0.0.0.0:0").await?;
+    let (sink, process) = TokioBatchUdpMetricSink::from(host, socket)?;
+
+    // Spawn the future!
+    let processing_job = spawn(process);
+
+    {
+        let client = StatsdClient::from_sink("my.metrics", sink);
+
+        // Emit metrics!
+        client.incr("some.counter");
+        client.time("some.methodCall", 42);
+        client.gauge("some.thing", 7);
+        client.meter("some.value", 5);
+
+        // the client drops here, and the sink along with it
+    }
+
+    // Wait for the processing job to complete!
+    processing_job.await.unwrap();
+    Ok(())
+}
+```
+
+Note that in order to ensure that all buffered metrics are submitted, the application must await
+the completion of the processing job *after* the client, as well as the sink along with it, are dropped.
+
+## License
+
+Licensed under the MIT license ([LICENSE-MIT](LICENSE-MIT) or http://opensource.org/licenses/MIT).
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+#![deny(missing_docs)]
+#![deny(clippy::pedantic)]
+
+//! A collection of Cadence [Metric Sink](https://docs.rs/cadence/0.22/cadence/trait.MetricSink.html)
+//! implementations that operate asynchronously using [Tokio](https://docs.rs/tokio/0.2/tokio).
+
 use cadence::{
     ErrorKind as MetricErrorKind,
     MetricError,
@@ -17,7 +23,13 @@ use std::{
         SocketAddr,
         ToSocketAddrs,
     },
+    panic::{
+        catch_unwind,
+        RefUnwindSafe,
+        UnwindSafe,
+    },
     pin::Pin,
+    process::abort,
 };
 
 use tokio::{
@@ -35,16 +47,76 @@ use tokio::{
     },
 };
 
-pub const DEFAULT_QUEUE_LEN: usize = 1024;
-pub const DEFAULT_BUF_SIZE: usize = 1432;
-pub const DEFAULT_MAX_DELAY_MS: u64 = 1000;
+/// Default length of metrics queue.
+pub const DEFAULT_QUEUE_CAPACITY: usize = 1024;
 
-#[derive(Debug)]
+/// Default size of buffer used for batching metrics.
+pub const DEFAULT_BATCH_BUF_SIZE: usize = 1432;
+
+/// Default maximum delay to wait before submitting accumulated metrics as a single batch,
+/// in milliseconds.
+pub const DEFAULT_MAX_BATCH_DELAY_MS: u64 = 1000;
+
+/// Metric sink that allows clients to enqueue metrics without blocking, and processing
+/// them asynchronously via UDP using Tokio runtime.
+///
+/// It also accumulates individual metrics for a configured maximum amount of time
+/// before submitting them as a single [batch](https://github.com/statsd/statsd/blob/master/docs/metric_types.md#multi-metric-packets).
+///
+/// Exceeding the configured queue capacity results in an error, which the client may handle as appropriate.
+///
+/// ## Important!
+/// The client is responsible for polling the asynchronous processing future, which is created along
+/// with the sink, in a manner appropriate for the application (e.g., spawning it in a Tokio task pool).
+///
+/// The client should also wait for this future to complete *after* dropping the metric sink.
+///
+/// ### Example
+///
+/// ```no_run
+/// use cadence::prelude::*;
+/// use cadence::{StatsdClient, DEFAULT_PORT};
+/// use tokio_cadence::TokioBatchUdpMetricSink;
+/// use tokio::{spawn, net::UdpSocket};
+///
+/// # #[tokio::main]
+/// # async fn main() -> cadence::MetricResult<()> {
+/// let host = ("metrics.example.com", DEFAULT_PORT);
+/// let socket = UdpSocket::bind("0.0.0.0:0").await?;
+/// let (sink, process) = TokioBatchUdpMetricSink::from(host, socket)?;
+///
+/// // Spawn the future!
+/// let processing_job = spawn(process);
+///
+/// {
+///     let client = StatsdClient::from_sink("my.metrics", sink);
+///
+///     // Emit metrics!
+///     client.incr("some.counter");
+///     client.time("some.methodCall", 42);
+///     client.gauge("some.thing", 7);
+///     client.meter("some.value", 5);
+///
+///     // the client drops here, and the sink along with it
+/// }
+///
+/// // Wait for the processing job to complete!
+/// processing_job.await.unwrap();
+/// # Ok(())
+/// # }
+/// ```
+#[derive(Clone, Debug)]
 pub struct TokioBatchUdpMetricSink {
     tx: Sender<Cmd>,
 }
 
+// we don't let tx panic
+impl UnwindSafe for TokioBatchUdpMetricSink {}
+impl RefUnwindSafe for TokioBatchUdpMetricSink {}
+
 impl TokioBatchUdpMetricSink {
+    /// Creates a new metric sink for the given statsd host using a previously bound UDP socket.
+    /// Other sink parameters are defaulted.
     pub fn from<T: ToSocketAddrs>(
         host: T,
         socket: UdpSocket,
@@ -55,16 +127,19 @@ impl TokioBatchUdpMetricSink {
         Self::with_capacity(
             host,
             socket,
-            DEFAULT_QUEUE_LEN,
-            DEFAULT_BUF_SIZE,
-            DEFAULT_MAX_DELAY_MS,
+            DEFAULT_QUEUE_CAPACITY,
+            DEFAULT_BATCH_BUF_SIZE,
+            DEFAULT_MAX_BATCH_DELAY_MS,
         )
     }
 
+    /// Creates a new metric sink for the given statsd host, using the UDP socket, as well as
+    /// metric queue capacity, batch buffer size, and maximum delay (in milliseconds) to wait
+    /// before submitting any accumulated metrics as a batch.
     pub fn with_capacity<T: ToSocketAddrs>(
         host: T,
         socket: UdpSocket,
-        queue_len: usize,
+        queue_capacity: usize,
         buf_size: usize,
         max_delay: u64,
     ) -> MetricResult<(
@@ -76,21 +151,35 @@ impl TokioBatchUdpMetricSink {
             MetricError::from((MetricErrorKind::InvalidInput, "No socket addresses yielded"))
         })?;
 
-        let (tx, rx) = channel(queue_len);
+        let (tx, rx) = channel(queue_capacity);
         let worker_fut = worker(rx, addr, socket, buf_size, max_delay);
 
         Ok((Self { tx }, Box::pin(worker_fut)))
     }
 
     fn try_send(&self, cmd: Cmd) -> Result<()> {
-        let mut tx = self.tx.clone();
-        if let Err(e) = tx.try_send(cmd) {
-            let kind = match e {
-                TrySendError::Full(_) => ErrorKind::WouldBlock,
-                TrySendError::Closed(_) => ErrorKind::Other,
-            };
+        // self.tx is !RefUnwindSafe -- don't let it panic!
+        let wrapped = catch_unwind(|| {
+            let mut tx = self.tx.clone();
+            tx.try_send(cmd)
+        });
 
-            return Err(Error::new(kind, e));
+        match wrapped {
+            Ok(res) => {
+                if let Err(e) = res {
+                    let kind = match e {
+                        TrySendError::Full(_) => ErrorKind::WouldBlock,
+                        TrySendError::Closed(_) => ErrorKind::Other,
+                    };
+
+                    return Err(Error::new(kind, e));
+                }
+            }
+
+            Err(e) => {
+                eprintln!("panic while attempting to enqueue statsd metric: {:?}", e);
+                abort();
+            }
         }
 
         Ok(())
@@ -109,7 +198,7 @@ impl MetricSink for TokioBatchUdpMetricSink {
     }
 }
 
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 enum Cmd {
     Write(String),
     Flush,
@@ -197,19 +286,13 @@ async fn worker(
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::io::{
-        Error,
-        ErrorKind,
-        Result,
-    };
-
     use tokio::{
         net::UdpSocket,
         spawn,
     };
 
     #[tokio::test]
-    async fn from() -> Result<()> {
+    async fn from() -> MetricResult<()> {
         let socket = UdpSocket::bind("0.0.0.0:0").await?;
         let result = TokioBatchUdpMetricSink::from("127.0.0.1:8125", socket);
 
@@ -219,7 +302,7 @@ mod tests {
     }
 
     #[tokio::test]
-    async fn from_bad_address() -> Result<()> {
+    async fn from_bad_address() -> MetricResult<()> {
         let socket = UdpSocket::bind("0.0.0.0:0").await?;
         let result = TokioBatchUdpMetricSink::from("bad address", socket);
 
@@ -229,7 +312,7 @@ mod tests {
     }
 
     #[tokio::test]
-    async fn emit() -> Result<()> {
+    async fn emit() -> MetricResult<()> {
         pretty_env_logger::try_init().ok();
 
         let mut server_socket = UdpSocket::bind("127.0.0.1:0").await?;
@@ -242,8 +325,7 @@ mod tests {
         debug!("local socket: {}", socket.local_addr()?);
 
         let (sink, fut) =
-            TokioBatchUdpMetricSink::from(format!("127.0.0.1:{}", server_addr.port()), socket)
-                .map_err(|e| Error::new(ErrorKind::Other, e))?;
+            TokioBatchUdpMetricSink::from(format!("127.0.0.1:{}", server_addr.port()), socket)?;
 
         let worker = spawn(fut);
 
@@ -265,12 +347,12 @@ mod tests {
         assert_eq!(MSG, String::from_utf8_lossy(&buf[..received]));
 
         drop(sink);
-        worker.await?;
+        worker.await.unwrap();
         Ok(())
     }
 
     #[tokio::test]
-    async fn emit_multi() -> Result<()> {
+    async fn emit_multi() -> MetricResult<()> {
         pretty_env_logger::try_init().ok();
 
         let mut server_socket = UdpSocket::bind("127.0.0.1:0").await?;
@@ -286,11 +368,10 @@ mod tests {
         let (sink, fut) = TokioBatchUdpMetricSink::with_capacity(
             format!("127.0.0.1:{}", server_addr.port()),
             socket,
-            DEFAULT_QUEUE_LEN,
+            DEFAULT_QUEUE_CAPACITY,
             BUF_SIZE,
-            DEFAULT_MAX_DELAY_MS,
-        )
-        .map_err(|e| Error::new(ErrorKind::Other, e))?;
+            DEFAULT_MAX_BATCH_DELAY_MS,
+        )?;
 
         let worker = spawn(fut);
 
@@ -326,7 +407,7 @@ mod tests {
         assert_eq!(MSG, String::from_utf8_lossy(&buf[..received]));
 
         drop(sink);
-        worker.await?;
+        worker.await.unwrap();
         Ok(())
     }
 }
